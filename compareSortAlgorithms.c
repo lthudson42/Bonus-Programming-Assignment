0ba384e5d0eebef492b4ca9a3c16f9fb28cc850c
LIAM HUDSON <lthudson42@gmail.com>
2024-03-24 23:50:08 -0400

    Extra Cred

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
@@ -30,34 +31,145 @@ size_t Size(void* ptr)
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+	int i, temp, front = 1;
+	for(i = n; i >= 2; i--){
+		temp = arr[1];
+		arr[1] = arr[i];
+		arr[i] = temp;
+		int max = front, l = front * 2, r = front*2+1, t2;
+		if(l <= i-1 && arr[l] > arr[max])
+			max = l;
+		if(r <= i-1 && arr[r] > arr[max])
+			max = r;
+		if(front != max){
+			t2 = arr[front];
+			arr[front] = arr[max];
+			arr[max] = t2;
+		}
+	}
 }
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
+
+	if(l < r){
+		// Declare variables
+		int mid = l + (r-1)/2;
+		
+
+		// Recursively call the mergeSort function
+		mergeSort(pData, l, mid);
+		mergeSort(pData, mid+1, r);
+
+		int i, j, k;
+		int n1 = mid - l + 1; // first sub-half
+		int n2 = r - mid; // second sub-half
+		
+		int *L = (int *)Alloc(n1 *sizeof(int));
+		int *R = (int *)Alloc(n2 * sizeof(int));
+
+
+		// Copy the values from pData into each temp array
+		for(i = 0; i < n1; i++)
+			L[i] = pData[l+i];
+		for(j = 0; j < n2; j++)
+			R[j] = pData[r+j];
+
+		// Re-initialize the index values to 0 (the beginning of the arrays)
+		i = 0;
+		j = 0;
+		k = l;
+		// Sort the sub-halfs and merge them into pData array
+		while(i < n1 && j < n2){
+			if(L[i] <= R[j]){
+				pData[k] = L[i];
+				i++;
+			}
+			else{
+				pData[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		// Place any remaining values in left temp array into pData
+		while(i < n1){
+			pData[k] = L[i];
+			i++;
+			k++;
+		}
+
+		// Place any remaining values in right temp array into pData
+		while(j < n2){
+			pData[k] = R[j];
+			j++;
+			k++;
+		}
+		DeAlloc(L);
+		DeAlloc(R);
+	}
 }
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	
+	int i, item, j;
+	for (i = 1; i < n; i++)
+	{
+		item = pData[i];
+		// Sort the elements through the array one index at a time if the element is greater than item
+		for(j=i-1; j>=0; j--)
+		{
+			if(pData[j]>item)
+				pData[j+1] = pData[j];
+			else
+				break;
+		}
+		pData[j+1] = item;
+	}
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+	int i, j, temp;
+	for (i = 0; i < n-1; i++)
+	{
+		for (j = 0; j < n-i-1; j++)
+		{
+			if (pData[j] > pData[j+1])
+			{ //then swap
+			temp = pData[j];
+			pData[j]=pData[j+1];
+			pData[j+1]=temp;
+			}
+		}
+	}
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+	int i, j, min_idx, temp;
+	// One by one move boundary of unsorted subarray
+	for (i = 0; i < n-1; i++)
+	{
+		// Find the minimum element in the unsorted array
+		min_idx = i;
+		for (j = i+1; j < n; j++)
+		if (pData[j] < pData[min_idx])
+		min_idx = j;
+		// Swap the found minimum element with the first element
+		temp = pData[i];
+		pData[i] = pData[min_idx];
+		pData[min_idx] = temp;
+
+	}
 }
 
 // parses input file to an integer array
@@ -71,7 +183,9 @@ int parseData(char *inputFileName, int **ppData)
 	{
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)Alloc(sizeof(int) * dataSz);
-		// Implement parse data block
+		for(int i = 0; i < dataSz; i++)
+			fscanf(inFile, "%d", &(*ppData)[i]);
+		fclose(inFile);
 	}
 	
 	return dataSz;
@@ -164,7 +278,7 @@ int main(void)
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
 		extraMemoryAllocated = 0;
 		start = clock();
-		heapSort(pDataCopy, 0, dataSz - 1);
+		heapSort(pDataCopy, dataSz - 1);
 		end = clock();
 		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
